07857f6df120643c225d60c4a29c38f2bb6a3bfa
jvsTSX <113801355+jvsTSX@users.noreply.github.com>
2023-05-07 23:37:18 -0300

    update to 1.1 format
    i forgot i had this file

--- ADPM_song_example_data.asm
+++ ADPM_song_example_data.asm
@@ -1,27 +1,72 @@
-DEFSECT   ".ADPMexample",  CODE
-SECT ".ADPMexample"
+DEFSECT    "MUSIC", CODE
+SECT    "MUSIC"
+db "HEADER"
+SongData:
 
-GLOBAL __SongData
-GLOBAL __SFXdata
-
-__SongData:
 ; HEADER -----------
 dw timeline0
 dw phrasesIndex0
 dw instrumentsIndex0
 dw macrosIndex0
-db 40h
+dw groovetable0
+db 00h ; song speed
+
+
+
+; INSTRUMENTS ------
+instrumentsIndex0:
+dw ins00
+dw ins01
+
+instrumentsData0:
+ins00:
+db  01h, 44h, 11011000b, 01h, 02h, 20h, 80h
+ins01:
+db 0FFh, 78h, 11001000b
+
+
+
+; MACROS -----------
+macrosIndex0:
+dw Gmacro00
+dw Gmacro01
+
+macrosData0:
+Gmacro00: ; drum
+db 10011111b, 20h, 7Fh
+db 10011110b, 18h, 7Fh
+db 10011110b, 14h, 7Fh
+db 10000010b
+db 01
+
+
+
+Gmacro01: ; slight pitch bend
+db 10001010b, 01
+db 10001010b, 00
+db 01h
+
+
+; GROOVE -----------
+groovetable0:
+db 04h
+db 03h
+db 03h
+db 03h
+db 0FFh, 00h
+
+
 
 ; TIMELINE ---------
 timeline0:
 
 ; now:
-db 00h, 00h ; phrase 0, tranpose by 0
-db 01h, 00h ; phrase 1, tranpose by 0
-db 01h, 02h ; phrase 1, tranpose by 2
-db 02h, 00h ; phrase 2, tranpose by 0
-db 01h, 00h ; phrase 1, tranpose by 0
-db 01h, 02h ; phrase 1, tranpose by 2
+db 00h, 01h ; phrase 0, tranpose by 0
+db 01h, 01h ; phrase 1, tranpose by 0
+db 01h, 03h ; phrase 1, tranpose by 2
+db 02h, 01h ; phrase 2, tranpose by 0
+db 01h, 01h ; phrase 1, tranpose by 0
+db 01h, 03h ; phrase 1, tranpose by 2
 db 03h ; (end phrase, only end command so garbage transpose don't matter)
 
 
@@ -35,211 +80,96 @@ dw phra03
 phrasesData0:
 phra00:
 
-db 01h,  34h, 00h,     90h, 00h
+db 01h,  34h, 00h,     50h, 00h
 
 db 00h,  04h, 01h
 db 00h,  90h
-db 00h,  32h, 00h,     90h, 00h
+db 00h,  32h, 00h,     50h, 00h
 db 00h, 0B1h
-db 00h, 0B2h,          32h 
+db 00h, 0B2h,         0C2h 
 db 00h, 0AFh
-db 00h,  04h, 01h,     90h, 00h
+db 00h,  04h, 01h,     50h, 00h
 db 00h,  90h
 db 00h,  2Dh, 00h
 db 00h,  10h, 01h
-db 00h,  2Ah, 00h,     90h, 00h
+db 00h,  2Ah, 00h,     50h, 00h
 db 00h,  04h, 01h
 db 00h,  2Dh, 00h
 db 00h, 0A8h
-db 00h,  04h, 01h,     90h, 00h
-db 50h,                32h
+db 00h,  04h, 01h,     50h, 00h
+db 20h,               0C2h
 db 00h,  90h
 db 00h,  84h
-db 00h,  28h, 00h,     90h, 00h
+db 00h,  28h, 00h,     50h, 00h
 db 00h,  04h, 01h
 db 01h,  90h
 
-db 00h,  28h, 00h,     90h, 00h
-db 50h,                32h
+db 00h,  28h, 00h,     50h, 00h
+db 20h,               0C2h
 db 00h,  10h, 01h
 db 00h,  25h, 00h
-db 50h,                 90h, 00h
+db 20h,                50h, 00h
 db 00h,  04h, 01h
 db 00h,  28h, 00h
 db 00h,  04h, 01h
 
-db 10h
+db 11h
 
 phra01:
-db 00h,  30h, 00h,     90h, 00h
+db 00h,  30h, 00h,     50h, 00h
 db 00h,  0Ch, 01h
 db 00h,  32h, 00h
 db 00h,  0Ch, 01h
-db 00h,  30h, 00h,     90h, 00h
+db 00h,  30h, 00h,     50h, 00h
 db 00h, 0AFh
 db 00h,  0Ch, 01h
 db 00h,  2Bh, 00h
-db 01h,  00h, 01h,     90h, 00h
+db 01h,  00h, 01h,     50h, 00h
 
 db 00h,  26h, 00h
-db 50h,                32h
-db 00h, 00h,  01h,     90h, 00h
-db 50h,                32h
+db 20h,               0C2h
+db 00h, 00h,  01h,     50h, 00h
+db 20h,               0C2h
 db 00h,  26h, 00h
 db 00h,  0Ch, 01h
 
-db 10h
+db 11h
 
 phra02:
-db 01h,  34h, 00h,     90h, 00h
+db 01h,  34h, 00h,     50h, 00h
 
 db 00h,  04h, 01h
 db 00h,  90h
-db 00h,  32h, 00h,     90h, 00h
+db 00h,  32h, 00h,     50h, 00h
 db 00h, 0B1h
-db 00h, 0B2h,          32h
+db 00h, 0B2h,         0C2h
 db 00h, 0AFh
-db 50h,                90h, 00h
+db 20h,                50h, 00h
 db 00h,  04h, 01h
 db 00h,  2Dh, 00h
 db 00h,  10h, 01h
-db 00h,  2Ah, 00h,     90h, 00h
+db 00h,  2Ah, 00h,     50h, 00h
 db 00h,  04h, 01h
 db 00h,  2Dh, 00h
 db 00h, 0B4h
-db 01h,  04h, 01h,     90h, 00h
+db 01h,  04h, 01h,     50h, 00h
  
 db 00h,  90h
-db 50h,                32h
-db 01h,  34h, 00h,     90h, 00h
+db 20h,               0C2h
+db 01h,  34h, 00h,     50h, 00h
 
 db 00h,  04h, 01h
-db 50h,                32h
-db 01h,  3Bh, 00h,     90h, 00h
+db 20h,               0C2h
+db 01h,  3Bh, 00h,     50h, 00h
  
 db 00h,  10h, 01h
 db 00h,  39h, 00h
-db 50h,                90h, 00h
+db 20h,                50h, 00h
 db 00h,  04h, 01h
 db 00h,  34h, 00h
 db 00h,  04h, 01h
 
-db 10h
+db 11h
 
 phra03:
-db 20h, 00h
-
-
-; INSTRUMENTS ------
-instrumentsIndex0:
-dw ins00
-dw ins01
-
-instrumentsData0:
-ins00:
-db 00h, 44h, 01h, 00010011b, 01h, 01h, 20h, 80h
-ins01:
-db 00h, 78h, 0FFh, 00000011b
-
-; MACROS -----------
-macrosIndex0:
-dw Gmacro00
-dw Gmacro01
-dw Gmacro02
-dw Gmacro03
-
-macrosData0:
-Gmacro00: ; drum
-db 01h, 20h,  7Bh, 80h,   20h
-db 01h, 18h,              20h
-db 01h, 14h,              20h
-db 00h, 00h,  70h, 00h,   30h, 0FFh
-
-Gmacro01: ; slight pitch bend
-db 00h, 001h,   20h
-db 00h, 0FFh,   30h, 0FFh
-
-Gmacro02:
-db 00h, 002h, 20h
-db 00h, 002h, 20h
-db 00h, 0FEh, 20h
-db 00h, 0FEh, 20h
-db 10h, 040h, 23h
-db 10h, 020h, 23h
-db 11h, 080h, 23h
-db 11h, 040h, 23h
-db 50h, 20h
-db 51h, 20h
-db 50h, 20h
-db 51h, 20h
-db 50h, 20h
-db 53h, 20h
-db 51h, 20h
-db 53h, 20h
-db 53h, 20h
-db 51h, 20h
-db 53h, 20h
-db 40h, 00h
-
-Gmacro03:
-db 20h
-db 00h, 002h ,20h
-db 00h, 0FEh ,20h
-db 30h, 0FAh
-
-
-
-; SFX EXAMPLE
-
-
-
-__SFXdata:
-; SFX index table
-dw SFX0
-dw SFX1
-dw SFX2
-dw SFX3
-
-SFX0: ; all of them
-db 10110011b, 0A0h, 30h
-db 10111011b, 80h, 8Fh
-db 10111011b, 88h, 8Fh
-db 10111010b, 70h, 8Fh
-db 10111010b, 78h, 8Fh
-db 10110011b, 80h, 30h
-db 10110011b, 88h, 34h
-db 10110010b, 70h, 38h
-db 10110010b, 78h, 40h
-db 00h
-
-SFX1: ; only duty
-db 10110011b, 80h, 38h
-db 10100011b, 70h
-db 10100011b, 60h
-db 10100011b, 50h
-db 10100011b, 40h
-db 10100011b, 30h
-db 00h
-
-SFX2: ; only freq (absol)
-db 10110011b, 80h, 30h
-db 10010011b, 024h
-db 10010011b, 020h
-db 10010011b, 028h
-db 10010011b, 02Eh
-db 10010011b, 02Eh
-db 00h
-
-SFX3: ; only freq (relative)
-db 10110011b, 80h, 30h
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 10011011b, 07Fh
-db 00h
+db 10h, 00h
